07cdf4fbfe15ba848c2855c71dcf17ba73c9a456
ronald.sequeira <ronald.sequeira@union.co>
2021-05-20 17:23:34 -0600

    Installed pg, aws-S3 upload provider and updated the config files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5cf56d83ce23884f3cf70bb6b98f82ea'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7abf7bb3-dfe9-47b7-9362-be09ee0292e1'
+};
