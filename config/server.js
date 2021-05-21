38cd2daf48a28b98019ccb62de7a86df21d1db28
ronald.sequeira <ronald.sequeira@union.co>
2021-05-21 16:17:20 -0600

    update url

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://api.ec2-54-197-206-4.compute-1.amazonaws.com',
+  url: 'http://ec2-54-197-206-4.compute-1.amazonaws.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '5cf56d83ce23884f3cf70bb6b98f82ea'),
